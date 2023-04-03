93852a7b7694e52c2a01fb0abbbec1115fef9ff6
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-04-03 17:21:35 +0900

    step 1

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main() {
+	char *line;
+	while (1) {
+		line = readline("minishell$ ");
+		free(line);
+	}
+}
