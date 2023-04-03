27a7cbac3fa3f826424dfa8d450e5a70b8640286
tfuji <tomofuji.dev@gmail.com>
2023-04-03 22:03:02 +0900

    step 2.1

--- src/main.c
+++ src/main.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int main() {
-	char *line;
+int	fatal_error(char *msg) __attribute__((noreturn));
+
+int	fatal_error(char *msg) {
+	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
+	exit(1);
+}
+
+int	interpret(char* line) {
+	extern char	**environ;
+	char		*argv[] = {line, NULL};
+	pid_t		pid;
+	int			wstatus;
+
+	pid = fork();
+	if (pid < 0)
+		fatal_error("fork");
+	// child process
+	else if (pid == 0) {
+		execve(line, argv, environ);
+		fatal_error("execve");
+	}
+	// parent process
+	else {
+		wait(&wstatus);
+		return (WEXITSTATUS(wstatus));
+	}
+}
+
+int	main() {
+	int		status;
+	char	*line;
 
 	rl_outstream = stderr;
 	while (1) {
@@ -13,7 +43,8 @@ int main() {
 			break;
 		if (*line)
 			add_history(line);
+		status = interpret(line);
 		free(line);
 	}
-	exit(0);
+	exit(status);
 }
