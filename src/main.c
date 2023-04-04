08e8be7811372b6d091ccf7162bfc4868dda95a9
tfuji <tomofuji.dev@gmail.com>
2023-04-04 11:22:21 +0900

    step 2.3

--- src/main.c
+++ src/main.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int	fatal_error(char *msg) __attribute__((noreturn));
+void	fatal_error(const char *msg) __attribute__((noreturn));
 
-int	fatal_error(char *msg) {
+void	fatal_error(const char *msg)
+{
 	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
 	exit(1);
 }
 
-int	interpret(char* line) {
+int	interpret(char *line)
+{
 	extern char	**environ;
 	char		*argv[] = {line, NULL};
 	pid_t		pid;
@@ -20,28 +23,35 @@ int	interpret(char* line) {
 	pid = fork();
 	if (pid < 0)
 		fatal_error("fork");
-	// child process
-	else if (pid == 0) {
+	else if (pid == 0)
+	{
+		// child process
 		execve(line, argv, environ);
 		fatal_error("execve");
 	}
-	// parent process
-	else {
+	else
+	{
+		// parent process
 		wait(&wstatus);
 		return (WEXITSTATUS(wstatus));
 	}
 }
 
-int	main() {
+int	main(void)
+{
 	int		status;
 	char	*line;
 
 	rl_outstream = stderr;
-	while (1) {
+	status = 0;
+	while (1)
+	{
 		line = readline("minishell$ ");
 		if (line == NULL)
-			break;
-		if (*line)
+			break ;
+		else if (!*line)
+			continue ;
+		else
 			add_history(line);
 		status = interpret(line);
 		free(line);
